e9ca0c0534c6b357ea53c9fd264d2f079750ad8e
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-12 19:07:40 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,11 @@
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(names_array)
+  badge_array =[]
+  names_array.each |name|
+  badge_array.push("Hello, my name is #{name}.")
+end
+    badge_array
+end
